c6c2644c3272a52a4709116d4f67a8d89f8adf77
Mark Dino <bluffin23@gmail.com>
2021-01-12 20:07:08 -0600

    Minor commit

---
+++ js/landing-page-secondary-links.js
@@ -0,0 +1,1 @@
+$(function(){$('#promos div').click(function(){if($(this).find('a').hasClass('button')){var sHref=$(this).find('a').attr('href');if(sHref){if($(this).find('a').hasClass('external')){window.open(sHref);return false;}else{window.location=sHref;}}else{return false;}}});$('#promos div').addClass('href');});
---
+++ js/landing-page-slideshow.js
@@ -0,0 +1,2 @@
+$(function(){$.fn.cycle.updateActivePagerLink=function(pager,currSlideIndex){$(pager).find('span').removeClass('current').filter('span:eq('+currSlideIndex+')').addClass('current');};$('#slideshow').cycle({prev:'#slideshow-prev',next:'#carousel-next',pager:'#slideshow-controls-div',pagerAnchorBuilder:function(idx,slide){var sReturn='<';sReturn+='span><';sReturn+='a href="#">&nbsp;<';sReturn+='/a><'
+sReturn+='/span>';return sReturn;}});});
